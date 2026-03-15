8d9c155faf31da0e461743d07d84093d8548849b
Jeffrey Lonzanida <jeffrey.lonzanida@acacia-soft.com>
2026-03-15 20:07:41 +0800

    save

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,22 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwindcss from "@tailwindcss/vite";
-
 import vercel from "@astrojs/vercel";
+import db from "@astrojs/db";
+import icon from "astro-icon";
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
+  site: import.meta.env.PUBLIC_APP_URL,
   vite: {
-      plugins: [tailwindcss()],
+      plugins: [
+        tailwindcss()
+      ],
   },
-
   adapter: vercel(),
+  integrations: [
+    icon(),
+    sitemap()
+  ],
 });
